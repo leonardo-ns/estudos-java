3eec162b1ccf43b20ee7340a87246dff5e6c475e
leonardo-ns <leonardonunessiqueira3@gmail.com>
2026-08-20 14:14:12 -0300

    Initial commit limpo

---
+++ SomaDeProdutos.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class SomaDeProdutos {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = 0 , b = 0 , soma = 0;
+        System.out.print("Digite o primeiro número: ");
+        a = sc.nextInt();
+        System.out.print("Digite o segundo número: ");
+        b = sc.nextInt();
+        soma = a + b;
+        System.out.println("A soma dos produtos é: " + soma);
+    }
+}
